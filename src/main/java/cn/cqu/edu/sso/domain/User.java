1123b0555c98003614c06e4e6972fb349ca23c3a
卓钊 <18350120264@163.com>
2021-10-17 20:03:41 +0800

    添加了user.java

---
+++ src/main/java/cn/cqu/edu/sso/domain/User.java
@@ -0,0 +1,24 @@
+//用户类
+public class User {
+    private String name;
+    private String pwd;
+    private String id;
+    public String getName(){
+        return name;
+    }
+    public String getPwd(){
+        return pwd;
+    }
+    public String getId(){
+        return id;
+    }
+    public void setName(String s) {
+        name = s;
+    }
+    public void setPwd(String s) {
+        pwd = s;
+    }
+    public void setId(String s) {
+        id = s;
+    }
+}
