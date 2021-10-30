fcaac145b4174e701f4a8d5cb22093c06213f885
卓钊 <18350120264@163.com>
2021-10-30 23:04:16 +0800

    数据库

--- src/main/java/cn/cqu/edu/sso/dao/VDataBase.java
+++ src/main/java/cn/cqu/edu/sso/dao/VDataBase.java
@@ -4,11 +4,18 @@ import cn.cqu.edu.sso.domain.User;
 import java.util.ArrayList;
 //虚拟数据库
 public class VDataBase {
-    private static ArrayList<User> userList = new ArrayList<User>()
-
-    public static boolean checkUser(User u){
+    private ArrayList<User> userList = new ArrayList<User>()
+    public VDataBase(){
+        User aWei = new User("aWei","123456");
+        User jieGe = new User("jieGe","123456");
+        User bingBing = new User("bingBing","123456");
+        userList.add(aWei);
+        userList.add(jieGe);
+        userList.add(bingBing);
+    }
+    public boolean checkUser(User u){
         return userList.contains(u);
-    } 
+    }
 
     public static void main(String[] args)throws Exception
     {
