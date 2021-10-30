507ea5647b4111d0f0539f74d9b2ee068593faec
卓钊 <18350120264@163.com>
2021-10-30 21:05:56 +0800

    修改

--- src/main/java/cn/cqu/edu/sso/controller/LoginController.java
+++ src/main/java/cn/cqu/edu/sso/controller/LoginController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import cn.cqu.edu.sso.domain.User;
@@ -12,7 +13,8 @@ import cn.cqu.edu.sso.domain.User;
 @RestController
 public class LoginController {
     @RequestMapping(value="/user/login",method = RequestMethod.POST)
-    public boolean test(String admin, String password)
+    public boolean test(@RequestParam(value = "admin") String admin, 
+    @RequestParam(value = "password") String password)
     {
     	
     	if(admin.equals("A") && password.equals("B")) {
