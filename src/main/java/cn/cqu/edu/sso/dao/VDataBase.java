3db11109afc5457f1562593f094ef565eb3002dd
卓钊 <18350120264@163.com>
2021-10-31 16:05:04 +0800

    jwt token生成

--- src/main/java/cn/cqu/edu/sso/dao/VDataBase.java
+++ src/main/java/cn/cqu/edu/sso/dao/VDataBase.java
@@ -1,29 +1,26 @@
 package cn.cqu.edu.sso.dao;
 
 import cn.cqu.edu.sso.domain.User;
-import java.util.ArrayList;
+import java.util.HashMap;
+
 //虚拟数据库
 public class VDataBase {
-    private ArrayList<User> userList = new ArrayList<User>();
-    public VDataBase(){
-        User aWei = new User("aWei","123456");
-        User jieGe = new User("jieGe","123456");
-        User bingBing = new User("bingBing","123456");
-        userList.add(aWei);
-        userList.add(jieGe);
-        userList.add(bingBing);
-    }
-    public boolean checkUser(User u){
-        for(int i=0;i<3;i++){
-            if(u.getName().equals(userList.get(i).getName()) && u.getPwd().equals(userList.get(i).getPwd())){
-                return true;
-            }
-        }
-        return userList.contains(u);
+    HashMap<String, String> userList = new HashMap<String, String>();
+
+    public VDataBase() {
+        userList.put("aWei", "123456");
+        userList.put("jieGe", "123456");
+        userList.put("bingBing", "123456");
     }
 
-    public static void main(String[] args)throws Exception
-    {
+    public boolean checkUser(User u) {
+        if (userList.containsKey(u.getName()) && u.getPwd().equals(userList.get(u.getName()))) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws Exception {
 
     }
 }
--- src/main/java/cn/cqu/edu/sso/service/LoginService.java
+++ src/main/java/cn/cqu/edu/sso/service/LoginService.java
@@ -1,17 +1,37 @@
 package cn.cqu.edu.sso.service;
 
 import cn.cqu.edu.sso.domain.User;
-import cn.cqu.edu.sso.dao.VDataBase;
+import cn.cqu.edu.sso.util.JwtUtil;
 
-public class LoginService{
+import com.alibaba.fastjson.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+import cn.hutool.core.bean.BeanUtil;
+import cn.cqu.edu.sso.dao.VDataBase;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+public class LoginService {
 
     VDataBase vDataBase;
 
-    public LoginService(){
+    public LoginService() {
         this.vDataBase = new VDataBase();
     }
 
-    public boolean loginPasswordCheck(User user){
-        return vDataBase.checkUser(user);
+    public JSONObject loginPasswordCheck(User user) {
+        JSONObject result = new JSONObject();
+        // 确认账号密码
+        if (vDataBase.checkUser(user)) {
+            Map<String, String> map = new HashMap<String, String>();
+            BeanUtil.copyProperties(user, map);
+            result.put("token", JwtUtil.getToken(map));
+        } else {
+            result.put("error", "账号或密码出错");
+        }
+        return result;
+    }
+
+    public String tokenCheck(String token) {
+        return JwtUtil.dateToString(JwtUtil.verifier(token).getExpiresAt());
     }
 }
